a053479814e11cad55b8f5ce0b814a101f966b81
MrNikita <mrnikita.harkov@gmail.com>
2020-08-23 12:15:10 +0300

    Registration page validation, message plugin for toast messages

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -19,6 +19,7 @@
           v-if="$v.email.$dirty && !$v.email.email"
         >Write correct Email</small>
       </div>
+
       <div class="input-field">
         <input
           id="password"
@@ -58,6 +59,8 @@
 
 <script>
 import {email, required, minLength} from 'vuelidate/lib/validators'
+import messages from '@/utils/messages'
+
 export default{
   name: 'login',
   data: () => ({
@@ -67,6 +70,11 @@ export default{
   validations:{
     email: {email, required},
     password: {required, minLength: minLength(6)}
+  },
+  mounted () {
+    if (messages [this.$route.query.message]) {
+      this.$message(messages [this.$route.query.message])
+    }
   },
   methods:{
     onSubmit(){
