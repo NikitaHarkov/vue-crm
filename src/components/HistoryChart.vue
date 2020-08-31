8506e74ff914f1f40c9ce70166db1925defd4360
MrNikita <mrnikita.harkov@gmail.com>
2020-08-31 15:38:49 +0300

    vue-chartjs in history page

---
+++ src/components/HistoryChart.vue
@@ -0,0 +1,44 @@
+<canvas></canvas>
+
+<script>
+import { Pie } from 'vue-chartjs'
+
+export default {
+  extends: Pie,
+  async mounted () {
+    const records = await this.$store.dispatch('fetchRecords')
+    const categories = await this.$store.dispatch('fetchCategories')
+    this.renderChart({
+      labels: categories.map(c => c.title),
+      datasets: [{
+        label: 'Расходы по категориям',
+        data: categories.map(c => {
+          return records.reduce((total, r) => {
+            if (r.categoryId === c.id && r.type === 'outcome') {
+              total += +r.amount
+            }
+            return total
+          }, 0)
+        }),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    })
+  }
+}
+</script>
--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <HistoryChart/>
     </div>
 
     <Loader v-if="loading"/>
@@ -16,7 +16,6 @@
 
     <section v-else>
       <HistoryTable :records="items"/>
-
       <Paginate
         v-model="page"
         :page-count="pageCount"
@@ -25,7 +24,7 @@
         :next-text="'Next'"
         :container-class="'pagination'"
         :page-class="'waves-effect'"
-        />
+      />
     </section>
   </div>
 </template>
@@ -33,6 +32,7 @@
 <script>
 import paginationMixin from '@/mixins/pagination.mixin'
 import HistoryTable from '@/components/HistoryTable'
+import HistoryChart from '@/components/HistoryChart'
 
 export default {
   name: 'history',
@@ -41,21 +41,30 @@ export default {
     loading: true,
     records: []
   }),
-  async mounted() {
+  async mounted () {
     this.records = await this.$store.dispatch('fetchRecords')
     const categories = await this.$store.dispatch('fetchCategories')
 
-    this.setupPagination(this.records.map(record => {
-      return {
-        ...record,
-        categoryName: categories.find(c => c.id === record.categoryId).title,
-        typeClass: record.type === 'income' ? 'green' : 'red',
-        typeText: record.type === 'income' ? 'Income' : 'Outcome'
-      }
-    }))
+    this.setup(categories)
+
     this.loading = false
   },
+  methods: {
+    setup (categories) {
+      this.setupPagination(this.records.map(record => {
+        return {
+          ...record,
+          categoryName: categories.find(c => c.id === record.categoryId).title,
+          typeClass: record.type === 'income' ? 'green' : 'red',
+          typeText: record.type === 'income' ? 'Income' : 'Outcome'
+        }
+      }))
+
+      this.loading = false
+    }
+  },
   components: {
+    HistoryChart,
     HistoryTable
   }
 }
